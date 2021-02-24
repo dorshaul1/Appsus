3ae240ad1f9cac72075f4054bfc379a8a9621869
Yonatan Mizrahi <yonatanmiz963@gmail.com>
2021-02-24 10:30:44 +0200

    first commit

---
+++ js/apps/appsus-pages/appsus-home-page.cmp.js
@@ -0,0 +1,12 @@
+export default {
+    template: `
+    <section class="home-page">
+        <h1 class="homepage-welcome">Welcome to</h1>
+        <h1 class="homepage-title">Appsus</h1>
+        <!-- <router-link class="buy-books-btn" to="/book">Lets </router-link> -->
+    </section>
+    `,
+    methods: {
+        
+    }
+}
