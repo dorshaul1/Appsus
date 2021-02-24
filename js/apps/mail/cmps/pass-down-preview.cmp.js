b77aa18606687dfdbf37477ae845ddc70666fe92
dorshaul1 <dor.asti@gmail.com>
2021-02-24 23:15:11 +0200

    adding mail

--- js/apps/mail/cmps/pass-down-preview.cmp.js
+++ js/apps/mail/cmps/pass-down-preview.cmp.js
@@ -13,18 +13,10 @@ export default {
             </div>
             <div class="from-pass-down-prev-content flex space-between align-items">
                     <div class="pass-down-prev-from flex"><h1>{{mail.from}}</h1><p>< {{mail.mailAdress}} ></p></div>
-                    <div class="pass-down-prev-date">{{dateToShow}}</div>
             </div>
             <div class="pass-down-prev-body-content flex center"><pre>{{mail.body}}</pre></div>
         </section>
     `,
-        computed:{
-            dateToShow() {
-                var ts = new Date(this.mail.sentAt);
-                new Date()
-                return `${ts.getMonth()+1} / ${ts.getDate()} / ${ts.getFullYear()} `
-            }
-        },
         methods: {
             fullScreen(mail){
                 this.$emit('fullScreen', mail)
