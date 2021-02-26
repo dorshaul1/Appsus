5678fcb52431dc557bd8e32f8e64b5dcbb321ef4
Yonatan Mizrahi <yonatanmiz963@gmail.com>
2021-02-27 00:53:23 +0200

    fixed pinned sign

--- js/apps/notes/cmps/note-img.cmp.js
+++ js/apps/notes/cmps/note-img.cmp.js
@@ -1,7 +1,7 @@
 export default {
     props: ['note'],
     template: `
-    <section class="note-img">
+    <section class="note-img flex align-items center">
         <img :src="note.info.url"> 
         <i class=" cat-icon fas fa-image"></i>
     </section>
